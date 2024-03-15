145aeccaf64f775cedd57350bbfe4ec816d314f4
吴伟强 <wuweiqiang@WuWQdeMacBook-Air.local>
2024-03-15 18:45:27 +0800

    complete lab4A

--- src/test.sh
+++ src/test.sh
@@ -2,7 +2,7 @@ n=1
 
 for ((i=1; i<=$n; i++)); do
     echo "Running iteration $i"
-    { go test -race ./raft && go test -race ./kvraft; } 2>&1 | tee -a test_result_$n.txt
+    { go test ./raft && go test ./kvraft; } 2>&1 | tee -a test_result_$n.txt
 done
 
 echo "All iterations completed. Check 'test_result_$n.txt' for details."
